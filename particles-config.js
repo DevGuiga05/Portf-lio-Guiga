9f8fd04223eb19829d06e26a5cf82cfa8c476fed
Guilherme Machado <devguiga05@gmail.com>
2025-04-10 01:56:28 -0300

    meu portfólio

---
+++ particles-config.js
@@ -0,0 +1,67 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 60,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#00bfff" // azul neon tech
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        }
+      },
+      opacity: {
+        value: 0.5,
+        random: false
+      },
+      size: {
+        value: 6, // TAMANHO DAS PARTÍCULAS
+        random: true
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#00bfff", // mesma cor das partículas
+        opacity: 0.4,
+        width: 2 // LINHAS MAIS GROSSAS
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        out_mode: "out"
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        },
+        resize: true
+      },
+      modes: {
+        repulse: {
+          distance: 100,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+  
